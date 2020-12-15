ac70e53e76045d0c9a827c7f40e24c7745c81333
NYAMUSI <nyamusimercy61@gmail.com>
2020-12-15 18:11:44 +0300

    Checking if list is palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+* is_palindrome - checks if list is palindrome
+* @head: list
+*
+* Return: 1 if palindrome, 0 if not
+*/
+int is_palindrome(listint_t **head)
+{
+	int i;
+	int j;
+	int myarray[100];
+
+	i = 0;
+	j = 0;
+	if (*head == NULL)
+	return (1);
+	while (*head)
+	{
+		myarray[j] = (*head)->n;
+		*head = (*head)->next;
+		j++;
+	}
+	j--;
+	while (i < j)
+	{
+		if (myarray[i] == myarray[j])
+		{
+			i++;
+			j--;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
