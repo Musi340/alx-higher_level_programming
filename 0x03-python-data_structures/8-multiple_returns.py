50f3ad5e35b011cd1cafd7e65b5adc22a541d2eb
NYAMUSI <nyamusimercy61@gmail.com>
2020-12-15 19:20:32 +0300

    Creating tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
+        l = len(sentence)
+        f = None
     else:
         l = len(sentence)
         f = sentence[0]
