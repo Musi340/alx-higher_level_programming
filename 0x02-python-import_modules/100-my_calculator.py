feab7346fa5f09543651c441cf727244d51c19e9
NYAMUSI <nyamusimercy61@gmail.com>
2020-12-16 03:31:22 +0300

    My Calaulator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,15 +4,17 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, div, mul
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
     if argv[2] == '+':
-        print("{:} + {:} = {:}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+        print("{:} + {:} = {:}".format(argv[1], argv[3], add(a, b)))
     elif argv[2] == '/':
-        print("{:} / {:} = {:}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+        print("{:} / {:} = {:}".format(argv[1], argv[3], div(a, b)))
     elif argv[2] == '-':
-        print("{:} - {:} = {:}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+        print("{:} - {:} = {:}".format(argv[1], argv[3], sub(a, b)))
     elif argv[2] == '*':
-        print("{:} * {:} = {:}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+        print("{:} * {:} = {:}".format(argv[1], argv[3], mul(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
