63ab84ccd6cf28ba6e73a8a5a6ccf19bf68ec4fb
NYAMUSI <nyamusimercy61@gmail.com>
2020-12-17 02:22:12 +0300

    Printing possible combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for i in range(9):
+for i in range(10):
     for j in range(10):
         if i < j and i != j:
-            if i != 8 and j != 9:
+            if i == 8 and j == 9:
+                print("89")
+            else:
                 print("{0:d}{1:d}, ".format(i, j), end="")
-print("89")
+
