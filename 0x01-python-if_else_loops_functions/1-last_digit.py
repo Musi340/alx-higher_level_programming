fc7c85facd80625e71065b9163766adb4fd32c36
NYAMUSI <nyamusimercy61@gmail.com>
2020-12-16 11:57:39 +0300

    Finding the last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+value = number
+if value < 0:
+    value *= -1
+k = value % 10
+if k < 6 and k != 0:
+    print("Last digit of {:} is {:} and is less than 6 and not 0".format(number, k))
+if k > 5:
+    print("Last digit of {:} is {:} and is greater than 5".format(number, k))
+if k == 0:
+    print("Last digit of {:} is {:} and is 0".format(number, k))
