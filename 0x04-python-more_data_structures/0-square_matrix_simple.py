cfeb87d98b084bd836354b139a4b3425f299dfa4
NYAMUSI <nyamusimercy61@gmail.com>
2020-12-17 20:34:52 +0300

    Squares of values in a multidimensional matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,4 +5,4 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         k = list(map(lambda x: x*x, matrix[i]))
         mylist = mylist + [k]
-    return mylist       
+    return mylist
