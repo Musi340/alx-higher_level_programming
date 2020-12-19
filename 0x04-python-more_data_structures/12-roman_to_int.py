c184feb0f3c532606bce8f7a78531746362a0a2e
NYAMUSI <nyamusimercy61@gmail.com>
2020-12-19 17:12:31 +0300

    Converting roman numerals to Integers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) != str:
+        return None
+    if not roman_string:
+        return None
+    mylist1 = [1, 5, 10, 50, 100, 500]
+    mylist2 = ['I', 'V', 'X', 'L', 'C', 'D']
+    mylist3 = []
+    mysum = 0
+    for i in roman_string:
+        for k in range(len(mylist2)):
+            if i == mylist2[k]:
+                mylist3.append(mylist1[k])
+    for m in range(len(mylist3) - 1):
+        if mylist3[m] < mylist3[m + 1]:
+            mysum += (mylist3[m]*-1)
+        else:
+            mysum += mylist3[m]
+    mysum += mylist3[len(mylist3) - 1]
+    return mysum
