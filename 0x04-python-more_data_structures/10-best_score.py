d58cb53c8441db2b6e808cbf10e69c925a4b8731
NYAMUSI <nyamusimercy61@gmail.com>
2020-12-19 15:17:56 +0300

    Finding key with the greatest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     mymax = 0
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
     for i in a_dictionary:
         if a_dictionary[i] > mymax:
