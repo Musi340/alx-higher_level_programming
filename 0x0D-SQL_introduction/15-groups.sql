8e50d0cdfea31f056e10b8e03e0d68885bcbc134
Mercy Nyamusi <nyamusimercy61@gmail.com>
2021-03-02 00:21:08 +0200

    Finding duplicate values in a table

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 --counting number of times values have been repeated.
-SELECT score FROM second_table ORDER BY score DESC HAVING COUNT(score) AS number;
+SELECT score FROM second_table ORDER BY score DESC HAVING COUNT(score) AS number ORDER BY score;
